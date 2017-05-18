f84ad3cc06e248cc86260cc9d406318901f965db
pbak95 <patryk.bak@gazeta.pl>
2017-05-18 12:45:18 +0200

    better register form

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -96,6 +96,13 @@ app.controller("registerCtrl", function($scope, $http) {
 			});
 			}
 		}
+		$scope.user.first_name = "";
+		$scope.user.last_name = "";
+		$scope.user.email = "";
+		$scope.user.username = "";
+		$scope.user.password = "";
+		$scope.user.confirm_password = "";
+		$scope.user.confirm_password = "";
 	}
 });
 
