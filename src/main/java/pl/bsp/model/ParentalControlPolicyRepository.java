cb9516227ce2718d670e6dfd07cd844fc37d2671
Kamil <kkamilstasiakk@gmail.com>
2017-05-22 21:53:26 +0200

    parental...

--- src/main/java/pl/bsp/model/ParentalControlPolicyRepository.java
+++ src/main/java/pl/bsp/model/ParentalControlPolicyRepository.java
@@ -3,9 +3,12 @@ package pl.bsp.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ParentalControlPolicyRepository extends JpaRepository<ParentalControlPolicy, Long>{
 	
-	public ParentalControlPolicy findById(long id);
+	ParentalControlPolicy findById(long id);
+	List<ParentalControlPolicy> findByUser(User user);
 }
