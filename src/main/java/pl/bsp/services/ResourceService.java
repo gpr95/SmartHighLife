c44bf875535e59dcd060028dcb3da9c430763f8f
kamilstasiakk <kkamilstasiakk@gmail.com>
2017-05-24 21:44:04 +0200

    findResourceByIdAndUsername

--- src/main/java/pl/bsp/services/ResourceService.java
+++ src/main/java/pl/bsp/services/ResourceService.java
@@ -1,11 +1,12 @@
 package pl.bsp.services;
 
 import pl.bsp.entities.Resource;
+import pl.bsp.model.User;
 
 public interface ResourceService {
 	
 	public boolean addResource(Resource resourceToAdd);
-	pl.bsp.model.Resource findByName(String name);
+	pl.bsp.model.Resource findBySerialIdAndUser(int serialId, User user);
 	public boolean deleteResource(int resourceId, String ownerUsername);
 
     boolean syncResource(int serialId, String userName);
